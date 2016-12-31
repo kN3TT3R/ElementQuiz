485d040e6949d2b2e2001d0bef571830969d8499
kN3TT3R <knetter@ptr-916yrbl8jp4st6yb1qx.18120a2.ip6.access.telenet.be>
2016-12-31 02:33:02 +0100

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by VDAB Cursist on 14/12/16.
+//  Copyright © 2016 knetter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
+    //MARK: - IBActions
+    @IBAction func gotoNextElement(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
