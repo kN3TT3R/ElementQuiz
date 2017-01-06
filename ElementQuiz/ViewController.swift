0058b567e872ad820dda9d738a6867b3c59c1977
VDAB Cursist <vdabcursist@vdabs-imac-6.local>
2017-01-06 12:01:30 +0100

    Final Version

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -10,33 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     // MARK: - IBOutlet
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
+
     
     // MARK: - Global Variables & Constants
     let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
     var currentElementIndex = 0
     
+    
     // MARK: - Overridden Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateElement()
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    
     //MARK: - IBActions
     @IBAction func gotoNextElement(_ sender: UIButton) {
-        
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
+    }
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = elementList[currentElementIndex]
     }
     
-    @IBAction func showAnswer(_ sender: UIButton) {
-        
-    }
     
     // MARK: Homemade Functions
     
+    /*  Sets the answer label to "?" and creates and sets the correct image.    */
+    func updateElement() {
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+            /*  Creates a new UIImage instance by looking in the asset catalog
+                for an image with a matching name   */
+        let image = UIImage(named: elementName)
+            /*  Sets the image of the image view to the newly created image instance    */
+        imageView.image = image
+    }
 }
 
